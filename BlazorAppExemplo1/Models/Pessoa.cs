fe9228614fa04c566ab73e6b44bf59542dc3beaa
Sayara Kaylaine <159396381+sayarakaylaine@users.noreply.github.com>
2024-02-28 09:59:08 -0400

    Add files via upload

---
+++ BlazorAppExemplo1/Models/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorAppExemplo1.Models
+{
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public string? Classificacao { get; set; }
+
+        public double CalcularIMC() //Implementar o cálculo do IMC
+        {
+            return Peso / (Altura * Altura);        
+        }
+
+        public string Classificar() //Implementar a classificação do cálculo de IMC
+        {
+            if (CalcularIMC() < 18.5)
+                return "Abaixo do peso";
+            else if (CalcularIMC() < 25)
+                return "Peso normal";
+            else if (CalcularIMC() < 30)
+                return "Sobrepeso";
+            else if (CalcularIMC() < 40)
+                return "Obesidade";
+            else if (CalcularIMC() > 40)
+                return "Obesidade grave";
+
+            Classificacao = Classificar();
+            return Nome +", você está"+ Classificacao;
+        }
+    }
+}
